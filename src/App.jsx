851cc3c3ce6ab333cfa3f3148aa7fa3f04cf1c6b
Guillermo Lagoria <guillermolagoria@gmail.com>
2023-02-12 13:32:08 -0300

    Terminado 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const App = () => {
   const [load, setLoad] = useState(0);
 
   useEffect(() => {
     const int = setInterval(() => {
-      setLoad(load => {
+      setLoad((load) => {
         if (load > 98) {
           clearInterval(int);
         }
@@ -15,14 +15,14 @@ const App = () => {
     }, 30);
 
     return () => {
-      clearInterval(int); 
+      clearInterval(int);
     };
   }, []);
 
   const scale = (num, in_min, in_max, out_min, out_max) => {
-    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
   };
-  //scale es una función que se está utilizando para escalar un número dado de un rango de entrada a un rango de salida. 
+  //scale es una función que se está utilizando para escalar un número dado de un rango de entrada a un rango de salida.
   //Es una función matemática simple que toma cinco argumentos: num, in_min, in_max, out_min y out_max.
 
   //num es el número que se desea escalar. in_min y in_max especifican el rango de entrada, es decir, el rango de valores que num puede tomar.
@@ -30,14 +30,16 @@ const App = () => {
 
   return (
     <div>
-      <section
-        className="bg"
-        style={{
-          filter: `blur(${scale(load, 0, 100, 20, 0)}px)`,
-          opacity: scale(load, 10, 0, 30, 0)
-        }}
-      />
+      <body>
+        <section
+          className="bg"
+          style={{
+            filter: `blur(${scale(load, 0, 100, 20, 0)}px)`,
+            opacity: scale(load, 10, 0, 30, 0),
+          }}
+        />
         <div className="loading-text">{`${load}%`}</div>
+      </body>
     </div>
   );
 };
